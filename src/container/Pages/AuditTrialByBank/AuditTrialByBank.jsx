4572511a9b5fb4a775d2ba9303a6f6fb4f4c3099
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2025-07-01 15:19:22 +0500

    All Pages for the Auditor has been completed

---
+++ src/container/Pages/AuditTrialByBank/AuditTrialByBank.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./AuditTrialByBank.module.css";
+const AuditTrialByBank = () => {
+  return <div>SaifAuditTrialBYbank</div>;
+};
+
+export default AuditTrialByBank;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "../container/Dashboard/index.jsx";
 import BopLogin from "../container/Pages/Login/BopLogin.jsx";
-
+import AuditTrialByCorporate from "../container/Pages/AuditTrialByCorporate/AuditTrialByCorporate.jsx";
+import AuditTrialByBank from "../container/Pages/AuditTrialByBank/AuditTrialByBank.jsx";
+import TradeCount from "../container/Pages/TradeCount/TradeCount.jsx";
+import ActivityByBank from "../container/Pages/ActivityByBank/ActivityByBank.jsx";
 
 const router = createBrowserRouter([
   {
@@ -17,7 +20,11 @@ const router = createBrowserRouter([
     path: "/BOP",
     element: <Dashboard />,
     children: [
-      { path: "audittrailbank", element: <div>Audit Trail Bank</div> },
+      { path: "audittrailbank", element: <AuditTrialByBank /> },
+      { path: "audittrailCorporate", element: <AuditTrialByCorporate /> },
+      { path: "TradeCount", element: <TradeCount /> },
+      { path: "ActivityByBank", element: <ActivityByBank /> },
+      { path: "ActivityByCorporate", element: <ActivityByBank /> },
     ],
   },
 ]);
