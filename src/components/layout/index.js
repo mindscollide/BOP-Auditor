958626c995b9daa65e0324f462ecf7a4535a0aae
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2025-07-01 12:24:13 +0500

    Initial commit
    Project scaffolding and initial file setup.

--- src/components/layout/index.js
+++
@@ -1,4 +0,0 @@
-import Header from "./Header/Header";
-import Sidebar from "./Sidebar/Sidebar";
-
-export { Header, Sidebar };
--- src/routes/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-const PrivateRoutes = () => {
-  let RoleID = localStorage.getItem("roleID");
-  const currentUser = JSON.parse(RoleID) === 6 ? true : false;
-  const token = localStorage.getItem("token");
-  return currentUser && token ? <Outlet /> : <Navigate to="*" />;
-};
-export default PrivateRoutes;
